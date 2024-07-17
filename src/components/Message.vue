aa8ba35762d4dc421f92b035ad2892eff2f10791
krystian-sikora <s223097@student.uek.krakow.pl>
2024-07-17 23:35:44 +0200

    message.senderName instead of finding matching id in contactStore (possible due to: https://github.com/krystian-sikora/chatsongs/commit/baa13cb52dea61ba003f2995e03db1f3f7246d3a)

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -3,14 +3,12 @@
 import { Avatar } from "@/components/ui/avatar/index.js";
 import { computed, onMounted, onUpdated, ref, watch } from "vue";
 
-const props = defineProps(['message', 'authRefs', 'contactRefs'])
+const props = defineProps(['message', 'authRefs'])
 
 const senderId = ref()
 const userId = ref()
 
 const isUserMessage = computed(() => props.message.senderId === props.authRefs.user.value?.id)
-
-const sender = computed(() => props.contactRefs.contacts.value?.find(c => c.id === senderId.value))
 
 onMounted(() => {
   evalMessage()
@@ -34,7 +32,7 @@ async function evalMessage() {
 <template>
   <div v-if="isUserMessage" class="float-right">
     <h1 class="inline-block border rounded-md my-2 p-2 max-w-[calc(100%-112px)] ml-[56px]">
-      {{props.message.content }}
+      {{ props.message.content }}
     </h1>
     <div class="inline-block float-right">
       <Avatar class="my-2 mx-2 flex drop-shadow">
@@ -42,15 +40,9 @@ async function evalMessage() {
       </Avatar>
     </div>
   </div>
-  <div v-else-if="sender">
-    <Avatar class="my-2 mx-2 float-left drop-shadow">
-      {{ sender.nickname.substring(0, 1) }}
-    </Avatar>
-    <h1 class="inline-block border rounded-md my-2 p-2 max-w-[calc(100%-112px)]"> {{ props.message.content }} </h1>
-  </div>
   <div v-else>
     <Avatar class="my-2 mx-2 float-left drop-shadow">
-      null
+      {{ props.message.senderName.substring(0, 1) }}
     </Avatar>
     <h1 class="inline-block border rounded-md my-2 p-2 max-w-[calc(100%-112px)]"> {{ props.message.content }} </h1>
   </div>
