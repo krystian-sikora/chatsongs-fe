d927edea003d5c30c1f3bb8a9a5f01e9e74a5d3b
krystian-sikora <s223097@student.uek.krakow.pl>
2024-09-07 20:07:41 +0200

    fix logo on small screens

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -5,9 +5,9 @@ import IconMusicSingleNote from "@/components/icons/IconMusicSingleNote.vue";
 
 <template>
   <div class="flex my-5 text-center">
-    <IconMusicSingleNote class="w-20"/>
-    <div class=" ">
-      <h1 class="p-1 text-6xl drop-shadow-[1px_1px_1px_rgba(255,255,255,1)] bg-gradient-to-br from-primary-700 to-rose-500 text-transparent bg-clip-text font-bold">
+    <IconMusicSingleNote class="w-16 md:w-20"/>
+    <div>
+      <h1 class="p-1 text-5xl md:text-6xl drop-shadow-[1px_1px_1px_rgba(255,255,255,1)] bg-gradient-to-br from-primary-700 to-rose-500 text-transparent bg-clip-text font-bold">
         Chatsongs
       </h1>
       <h2 class="p-1 drop-shadow-[1px_1px_1px_rgba(255,255,255,1)] bg-gradient-to-br from-primary-700 to-rose-500 text-transparent bg-clip-text">
